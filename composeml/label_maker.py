501c48b3b08d42c608fa28058d5c52a86c567468
Jeff Hernandez <inboxjeff@outlook.com>
2019-05-24 10:04:22 -0500

    minor update

--- composeml/label_maker.py
+++ composeml/label_maker.py
@@ -49,7 +49,7 @@ def on_slice(make_label, window, min_data, gap, n_examples):
             label = make_label(df[:time], *args, **kwargs)
 
             not_none = label is not None
-            not_nan = label is pd.np.nan
+            not_nan = label is not pd.np.nan
             if not_none or not_nan:
                 labels[cutoff_time] = label
 
