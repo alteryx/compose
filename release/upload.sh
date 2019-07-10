88a4e4beec85f00e99fe9b475f0f77ea2aeffae1
Jeff Hernandez <inboxjeff@outlook.com>
2019-07-10 18:55:56 -0500

    update

--- release/upload.sh
+++ release/upload.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-echo "REPO: $GITHUB_REPOSITORY, REF: $GITHUB_REF"
 
-# Checkout specified commit
-git checkout "$TAG"
+published=$(python -c "
+import json
+file = open('$GITHUB_EVENT_PATH', 'r')
+event = json.load(file)
+published = event.get('action') == published
+print(published)")
 
-# Remove build artifacts
-rm -rf .eggs/ rm -rf dist/ rm -rf build/
+echo $published
 
-# Create distributions
-python setup.py sdist bdist_wheel
+# # Checkout specified commit
+# git checkout "$TAG"
 
-# Install twine, module used to upload to pypi
-pip install --user twine -q
+# # Remove build artifacts
+# rm -rf .eggs/ rm -rf dist/ rm -rf build/
 
-# Upload to pypi or testpypi
-echo "Upoading $TAG to pypitest ..."
-# python -m twine upload dist/* -r "pypitest"
+# # Create distributions
+# python setup.py sdist bdist_wheel
+
+# # Install twine, module used to upload to pypi
+# pip install --user twine -q
+
+# # Upload to pypi or testpypi
+# echo "Upoading $TAG to pypitest ..."
+# # python -m twine upload dist/* -r "pypitest"
