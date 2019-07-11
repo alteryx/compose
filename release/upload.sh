4e6a6b8999ebae8cef0780877691ebfe61890eb3
Jeff Hernandez <inboxjeff@outlook.com>
2019-07-11 11:54:07 -0500

    undo twine skip existing

--- release/upload.sh
+++ release/upload.sh
@@ -30,7 +30,7 @@ upload_to_pypi () {
     pip install --user twine -q
 
     # Upload to pypi or testpypi
-    python -m twine upload dist/* --skip-existing \
+    python -m twine upload dist/* \
     --username $PYPI_USERNAME --password $PYPI_PASSWORD \
     --repository-url $TWINE_REPOSITORY_URL
 }
