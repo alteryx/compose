8e3cba7aa80a75a32341baba334b1be37238e037
Jeff Hernandez <inboxjeff@outlook.com>
2019-07-11 11:21:42 -0500

    update

--- release/upload.sh
+++ release/upload.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-upload_to_pypi () {
-    # Get tag from environment
-    tag=$(basename $GITHUB_REF)
+# Get tag from environment
+tag=$(basename $GITHUB_REF)
 
+# Get action that triggered release event
+action=$(python -c "
+import json
+
+with open('$GITHUB_EVENT_PATH', 'r') as file:
+    event = json.load(file)
+    action = event.get('action')
+
+print(action)
+")
+
+echo "Release $tag was $action on GitHub ..."
+
+upload_to_pypi () {
     # Checkout specified tag
     git checkout tags/$tag
 
@@ -17,24 +30,10 @@ upload_to_pypi () {
     pip install --user twine -q
 
     # Upload to pypi or testpypi
-    echo "Uploading $tag to $TWINE_REPOSITORY_URL ..."
     python -m twine upload dist/* \
     --username $PYPI_USERNAME --password $PYPI_PASSWORD \
     --repository-url $TWINE_REPOSITORY_URL
 }
-
-# Get action that triggered release event
-action=$(python -c "
-import json
-
-with open('$GITHUB_EVENT_PATH', 'r') as file:
-    event = json.load(file)
-    action = event.get('action')
-
-print(action)
-")
-
-echo "Release was $action on GitHub ..."
 
 # If release was published on GitHub then upload to PyPI
 if [ $action = "published" ]; then upload_to_pypi; fi
