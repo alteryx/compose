9564d030126533679213bce95c5677fa8ffa8f5f
Jeff Hernandez <inboxjeff@outlook.com>
2019-07-11 10:38:05 -0500

    update

--- release/upload.sh
+++ release/upload.sh
@@ -1,35 +1,40 @@
 #!/bin/sh
 
-# Get action that triggered release event
-release=$(python -c "
-import json
-
-with open('$GITHUB_EVENT_PATH', 'r') as file:
-    event = json.load(file)
-    release = event.get('action')
-
-print(release)
-")
-
 upload_to_pypi () {
-    # Checkout specified commit
-    git checkout tags/$GITHUB_REF
+    # Get tag from environment
+    tag=$(basename $GITHUB_REF)
+
+    # Checkout specified tag
+    git checkout tags/$tag
 
     # Remove build artifacts
     rm -rf .eggs/ rm -rf dist/ rm -rf build/
 
     # Create distributions
-    python setup.py sdist bdist_wheel
+    python setup.py sdist bdist_wheel -q
 
     # Install twine, module used to upload to pypi
     pip install --user twine -q
 
     # Upload to pypi or testpypi
-    echo "Uploading $GITHUB_REF to $TWINE_REPOSITORY_URL ..."
+    echo "Uploading $tag to $TWINE_REPOSITORY_URL ..."
     TWINE_USERNAME=$PYPI_USERNAME
     TWINE_PASSWORD=$PYPI_PASSWORD
-    twine upload dist/*
+    python -m twine upload dist/*
 }
 
+# Get action that triggered release event
+action=$(python -c "
+import json
+
+with open('$GITHUB_EVENT_PATH', 'r') as file:
+    event = json.load(file)
+    action = event.get('action')
+
+print(action)
+")
+
+echo "Release was $action on GitHub ..."
+
 # If release was published on GitHub then upload to PyPI
-if [ $release = "published" ]; then upload_to_pypi; fi
+if [ $action = "published" ]; then upload_to_pypi; fi
