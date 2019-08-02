aee7cc1678a0f56df22c7d63ab30104d2456c9fb
Jeff Hernandez <inboxjeff@outlook.com>
2019-08-02 13:03:51 -0500

    update test

--- composeml/tests/test_label_transforms/test_sample.py
+++ composeml/tests/test_label_transforms/test_sample.py
@@ -7,7 +7,7 @@ def labels(labels):
 
 
 def test_sample_n_int(labels):
-    given_answer = labels.sample(n=2, random_state=0)
+    given_answer = labels.sample(n=2, random_state=0).sort_index()
     given_answer = given_answer.to_csv(index=True).splitlines()
 
     answer = [
@@ -21,26 +21,25 @@ def test_sample_n_int(labels):
 
 def test_sample_n_dict(labels):
     n = {True: 1, False: 2}
-    given_answer = labels.sample(n=n, random_state=0)
+    given_answer = labels.sample(n=n, random_state=0).sort_index()
     given_answer = given_answer.to_csv(index=True).splitlines()
 
     answer = [
         'label_id,customer_id,cutoff_time,my_labeling_function',
+        '1,1,2014-01-01 00:48:00,False',
         '2,2,2014-01-01 00:01:00,True',
         '3,2,2014-01-01 00:04:00,False',
-        '1,1,2014-01-01 00:48:00,False',
     ]
 
     assert given_answer == answer
 
 
 def test_sample_frac_int(labels):
-    given_answer = labels.sample(frac=.5, random_state=1)
+    given_answer = labels.sample(frac=.25, random_state=0).sort_index()
     given_answer = given_answer.to_csv(index=True).splitlines()
 
     answer = [
         'label_id,customer_id,cutoff_time,my_labeling_function',
-        '3,2,2014-01-01 00:04:00,False',
         '2,2,2014-01-01 00:01:00,True',
     ]
 
@@ -49,13 +48,13 @@ def test_sample_frac_int(labels):
 
 def test_sample_frac_dict(labels):
     frac = {True: 1., False: .5}
-    given_answer = labels.sample(frac=frac, random_state=0)
+    given_answer = labels.sample(frac=frac, random_state=0).sort_index()
     given_answer = given_answer.to_csv(index=True).splitlines()
 
     answer = [
         'label_id,customer_id,cutoff_time,my_labeling_function',
-        '2,2,2014-01-01 00:01:00,True',
         '0,1,2014-01-01 00:45:00,True',
+        '2,2,2014-01-01 00:01:00,True',
         '3,2,2014-01-01 00:04:00,False',
     ]
 
