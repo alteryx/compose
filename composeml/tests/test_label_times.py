a9aa950ace69b986c127e0e3218bbebfc4e7f102
Jeff Hernandez <inboxjeff@outlook.com>
2019-08-02 09:46:37 -0500

    Revert "test describe"
    This reverts commit 3ceb69a8132ef16fe999dff87f94c4f8ca5e7476.

--- composeml/tests/test_label_times.py
+++ composeml/tests/test_label_times.py
@@ -1,12 +1,3 @@
-from ..label_times import LabelTimes
-
-
 def test_describe(labels):
     labels = labels.bin(2)
     assert labels.describe() is None
-
-
-def test_describe_empty():
-    data = {"label": [100, 200]}
-    label_times = LabelTimes(data, name="label")
-    label_times.describe()
