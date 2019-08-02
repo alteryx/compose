5a0751c11147ce4981781518f87f2b747fc2ca5b
Jeff Hernandez <inboxjeff@outlook.com>
2019-08-02 13:15:33 -0500

    update test

--- composeml/tests/test_label_times.py
+++ composeml/tests/test_label_times.py
@@ -1,13 +1,9 @@
-from ..label_times import LabelTimes
-
-
 def test_describe(labels):
     labels = labels.bin(2)
     labels.settings.update(num_examples_per_instance=2)
     assert labels.describe() is None
 
 
-def test_describe_empty():
-    data = {'labels': [1, 2]}
-    label_times = LabelTimes(data, name="labels")
-    label_times.describe()
+def test_describe_empty(labels):
+    labels.settings.clear()
+    assert labels.describe() is None
