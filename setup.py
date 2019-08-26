c3f4a2c6aeebcd321c2f98f95e33b9ad8a1c3a6f
Jeff Hernandez <inboxjeff@outlook.com>
2019-08-26 16:15:54 -0500

    Update Doc Reqs (#55)

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     tests_require=open('test-requirements.txt').readlines(),
     long_description=open('README.md').read(),
     packages=find_packages(),
-    package_data={'composeml': ['datasets/*.csv']},
+    package_data={'composeml': ['demos/*.csv']},
     include_package_data=True,
 )
