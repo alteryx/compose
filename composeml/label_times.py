6ea113f84187377efdbb56c8a216c6d56399b5de
Jeff Hernandez <inboxjeff@outlook.com>
2019-09-15 20:15:13 -0400

    Count per Group (#70)

--- composeml/label_times.py
+++ composeml/label_times.py
@@ -60,6 +60,14 @@ class LabelTimes(pd.DataFrame):
             return distribution
 
     @property
+    def count(self):
+        """Returns label count per instance."""
+        count = self.groupby(self.target_entity)
+        count = count[self.name].count()
+        count = count.to_frame('count')
+        return count
+
+    @property
     def count_by_time(self):
         """Returns label count across cutoff times."""
         if self.is_discrete:
