589bbfced60659d8ec9e0d5b795807142f773e03
Jeff Hernandez <inboxjeff@outlook.com>
2019-10-10 14:47:01 -0500

    parser

--- composeml/offsets.py
+++ composeml/offsets.py
@@ -2,6 +2,31 @@ import pandas as pd
 
 from composeml.utils import can_be_type
 
+import re
+
+
+def parse(value):
+    """Maps text to offset.
+
+    Args:
+        value (str) : Text description of offset.
+
+    Returns:
+        BaseOffset : Instance of offset.
+    """
+    pattern = re.compile('until start of next (?P<unit>[a-z]+)')
+    match = pattern.search(value.lower())
+
+    if match:
+        match = match.groupdict()
+        unit = match['unit']
+
+        if unit == 'month':
+            return pd.offsets.MonthBegin()
+
+        if unit == 'year':
+            return pd.offsets.YearBegin()
+
 
 def is_offset(value):
     """Checks whether a value is an offset.
@@ -29,10 +54,13 @@ def to_offset(value):
         offset = value
 
     elif isinstance(value, str):
-        error = 'offset must be a valid string'
-        assert can_be_type(type=pd.tseries.frequencies.to_offset, string=value), error
-        offset = pd.tseries.frequencies.to_offset(value)
-        assert offset.n > 0, 'offset must be greater than zero'
+        offset = parse(value)
+
+        if offset is None:
+            error = 'offset must be a valid string'
+            assert can_be_type(type=pd.tseries.frequencies.to_offset, string=value), error
+            offset = pd.tseries.frequencies.to_offset(value)
+            assert offset.n > 0, 'offset must be greater than zero'
 
     else:
         assert is_offset(value), 'invalid offset'
