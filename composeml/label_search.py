9a25d0155d4d53a71c661c47b4d456cd2d780371
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-04-20 10:33:30 -0500

    restructure label search classes

--- composeml/label_search.py
+++ composeml/label_search.py
@@ -1,40 +1,31 @@
-from abc import ABC, abstractmethod
 from pandas import isnull
 
 
-class LabelMakerSearch(ABC):
-    @property
-    @abstractmethod
-    def total_examples(self):
-        return NotImplementedError("This search must have `total_examples` as a property.")
-
-    @property
-    @abstractmethod
-    def is_search_complete(self):
-        return NotImplementedError("This search must have `is_search_complete` as a property.")
-
-    @property
-    @abstractmethod
-    def is_finite_search(self):
-        return NotImplementedError("This search must have `is_finite_search` as a property.")
-
-    def is_label_valid(self, label):
-        return NotImplementedError("This search must have `is_label_valid` as a method.")
-
-    @abstractmethod
-    def update_count(self, labels):
-        return NotImplementedError("This search must have `is_label_valid` as a method.")
-
-    @abstractmethod
-    def reset_count(self):
-        return NotImplementedError("This search must have `is_label_valid` as a method.")
-
-
-class ExampleSearch(LabelMakerSearch):
+class ExampleSearch:
     def __init__(self, expected_example_count):
         pass
 
-
-class LabelSearch(LabelMakerSearch):
-    def __init__(self, expected_label_counts):
+    @property
+    def total_examples(self):
         pass
+
+    @property
+    def is_search_complete(self):
+        pass
+
+    @property
+    def is_finite_search(self):
+        pass
+
+    def is_label_valid(self, label):
+        return not isnull(label)
+
+    def update_count(self, labels):
+        pass
+
+    def reset_count(self):
+        pass
+
+
+class LabelSearch(ExampleSearch):
+    pass
