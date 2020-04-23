9e9387b3c3efe4771bea6bcbd4d5ed8bec89d036
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-04-23 14:14:53 -0500

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='composeml',
-    version='0.1.8',
+    version='0.2.0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
     license='BSD 3-clause',
