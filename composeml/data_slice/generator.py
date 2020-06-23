2edfb95ae025c1fbf5fd6fa7c95dc3dc122dd8b1
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-06-23 09:16:07 -0500

    removes unused code

--- composeml/data_slice/generator.py
+++ composeml/data_slice/generator.py
@@ -1,7 +1,6 @@
 import composeml.data_slice.extension
 import pandas as pd
 
-from composeml.data_slice import DataSlice, DataSliceContext
 from composeml.data_slice.offset import to_offset
 from composeml.utils import can_be_type
 
@@ -21,13 +20,6 @@ class DataSliceGenerator:
             drop_empty=self.drop_empty,
         )
 
-        # data_slices = self._slice_by_time(
-        #     df=df,
-        #     gap=self.gap,
-        #     min_data=self.min_data,
-        #     drop_empty=self.drop_empty,
-        # )
-
         for ds in data_slices:
             yield ds
 
@@ -42,129 +34,3 @@ class DataSliceGenerator:
             window_size = to_offset(window_size)
 
         self.window_size = window_size
-
-    def _cutoff_data(self, df, threshold):
-        """Cuts off data before the threshold.
-
-        Args:
-            df (DataFrame): Data frame to cutoff data.
-            threshold (int or str or Timestamp): Threshold to apply on data.
-                If integer, the threshold will be the time at `n + 1` in the index.
-                If string, the threshold can be an offset or timestamp.
-                An offset will be applied relative to the first time in the index.
-
-        Returns:
-            df, cutoff_time (tuple(DataFrame, Timestamp)): Returns the data frame and the applied cutoff time.
-        """
-        if isinstance(threshold, int):
-            assert threshold > 0, 'threshold must be greater than zero'
-            df = df.iloc[threshold:]
-
-            if df.empty:
-                return df, None
-
-            cutoff_time = df.index[0]
-
-        elif isinstance(threshold, str):
-            if can_be_type(type=pd.tseries.frequencies.to_offset, string=threshold):
-                threshold = pd.tseries.frequencies.to_offset(threshold)
-                assert threshold.n > 0, 'threshold must be greater than zero'
-                cutoff_time = df.index[0] + threshold
-
-            elif can_be_type(type=pd.Timestamp, string=threshold):
-                cutoff_time = pd.Timestamp(threshold)
-
-            else:
-                raise ValueError('invalid threshold')
-
-        else:
-            is_timestamp = isinstance(threshold, pd.Timestamp)
-            assert is_timestamp, 'invalid threshold'
-            cutoff_time = threshold
-
-        if cutoff_time != df.index[0]:
-            df = df[df.index >= cutoff_time]
-
-            if df.empty:
-                return df, None
-
-        return df, cutoff_time
-
-    def _slice_by_time(self, df, gap=None, min_data=None, drop_empty=True):
-        """Generate data slices for a group.
-
-        Args:
-            df (DataFrame): Data frame to generate data slices.
-            gap (str or int): Time between examples. Default value is window size.
-                If an integer, search will start on the first event after the minimum data.
-            min_data (int or str or Timestamp): Threshold to cutoff data.
-            drop_empty (bool): Whether to drop empty slices. Default value is True.
-
-        Returns:
-            df_slice (generator): Returns a generator of data slices.
-        """
-        df = df.loc[df.index.notnull()]
-        assert df.index.is_monotonic_increasing, "Please sort your dataframe chronologically before calling search"
-
-        if df.empty:
-            return
-
-        threshold = min_data or df.index[0]
-        df, cutoff_time = self._cutoff_data(df=df, threshold=threshold)
-
-        if df.empty:
-            return
-
-        if isinstance(gap, int):
-            cutoff_time = df.index[0]
-
-        df = DataSlice(df)
-        df.context = DataSliceContext(slice_number=0)
-
-        def iloc(index, i):
-            if i < index.size:
-                return index[i]
-
-        while not df.empty and cutoff_time <= df.index[-1]:
-            if isinstance(self.window_size, int):
-                df_slice = df.iloc[:self.window_size]
-                window_end = iloc(df.index, self.window_size)
-
-            else:
-                window_end = cutoff_time + self.window_size
-                df_slice = df[:window_end]
-
-                # Pandas includes both endpoints when slicing by time.
-                # This results in the right endpoint overlapping in consecutive data slices.
-                # Resolved by making the right endpoint exclusive.
-                # https://pandas.pydata.org/pandas-docs/version/0.19/gotchas.html#endpoints-are-inclusive
-
-                if not df_slice.empty:
-                    overlap = df_slice.index == window_end
-                    if overlap.any():
-                        df_slice = df_slice[~overlap]
-
-            df_slice.context.window = (cutoff_time, window_end)
-
-            if isinstance(gap, int):
-                gap_end = iloc(df.index, gap)
-                df_slice.context.gap = (cutoff_time, gap_end)
-                df = df.iloc[gap:]
-
-                if not df.empty:
-                    cutoff_time = df.index[0]
-
-            else:
-                gap_end = cutoff_time + gap
-                df_slice.context.gap = (cutoff_time, gap_end)
-                cutoff_time += gap
-
-                if cutoff_time <= df.index[-1]:
-                    df = df[cutoff_time:]
-
-            if df_slice.empty and drop_empty:
-                continue
-
-            df.context.slice_number += 1
-
-            yield df_slice
--- composeml/label_maker.py
+++ composeml/label_maker.py
@@ -3,68 +3,14 @@ from sys import stdout
 import pandas as pd
 from tqdm import tqdm
 
-from composeml.data_slice import (
-    DataSlice,
-    DataSliceContext,
-    DataSliceGenerator
-)
+from composeml.data_slice import (DataSlice, DataSliceContext, DataSliceGenerator)
 from composeml.data_slice.offset import to_offset
 from composeml.label_search import ExampleSearch, LabelSearch
 from composeml.label_times import LabelTimes
-from composeml.utils import can_be_type
-
-
-def cutoff_data(df, threshold):
-    """Cuts off data before the threshold.
-
-    Args:
-        df (DataFrame): Data frame to cutoff data.
-        threshold (int or str or Timestamp): Threshold to apply on data.
-            If integer, the threshold will be the time at `n + 1` in the index.
-            If string, the threshold can be an offset or timestamp.
-            An offset will be applied relative to the first time in the index.
-
-    Returns:
-        df, cutoff_time (tuple(DataFrame, Timestamp)): Returns the data frame and the applied cutoff time.
-    """
-    if isinstance(threshold, int):
-        assert threshold > 0, 'threshold must be greater than zero'
-        df = df.iloc[threshold:]
-
-        if df.empty:
-            return df, None
-
-        cutoff_time = df.index[0]
-
-    elif isinstance(threshold, str):
-        if can_be_type(type=pd.tseries.frequencies.to_offset, string=threshold):
-            threshold = pd.tseries.frequencies.to_offset(threshold)
-            assert threshold.n > 0, 'threshold must be greater than zero'
-            cutoff_time = df.index[0] + threshold
-
-        elif can_be_type(type=pd.Timestamp, string=threshold):
-            cutoff_time = pd.Timestamp(threshold)
-
-        else:
-            raise ValueError('invalid threshold')
-
-    else:
-        is_timestamp = isinstance(threshold, pd.Timestamp)
-        assert is_timestamp, 'invalid threshold'
-        cutoff_time = threshold
-
-    if cutoff_time != df.index[0]:
-        df = df[df.index >= cutoff_time]
-
-        if df.empty:
-            return df, None
-
-    return df, cutoff_time
 
 
 class LabelMaker:
     """Automatically makes labels for prediction problems."""
-
     def __init__(self, target_entity, time_index, labeling_function=None, window_size=None, label_type=None):
         """Creates an instance of label maker.
 
@@ -129,85 +75,6 @@ class LabelMaker:
         assert isinstance(value, dict), 'value type for labeling function not supported'
         self._labeling_function = value
 
-    def _slice_by_time(self, df, gap=None, min_data=None, drop_empty=True):
-        """Generate data slices for a group.
-
-        Args:
-            df (DataFrame): Data frame to generate data slices.
-            gap (str or int): Time between examples. Default value is window size.
-                If an integer, search will start on the first event after the minimum data.
-            min_data (int or str or Timestamp): Threshold to cutoff data.
-            drop_empty (bool): Whether to drop empty slices. Default value is True.
-
-        Returns:
-            df_slice (generator): Returns a generator of data slices.
-        """
-        df = df.loc[df.index.notnull()]
-        assert df.index.is_monotonic_increasing, "Please sort your dataframe chronologically before calling search"
-
-        if df.empty:
-            return
-
-        threshold = min_data or df.index[0]
-        df, cutoff_time = cutoff_data(df=df, threshold=threshold)
-
-        if df.empty:
-            return
-
-        if isinstance(gap, int):
-            cutoff_time = df.index[0]
-
-        df = DataSlice(df)
-        df.context = DataSliceContext(slice_number=0, target_entity=self.target_entity)
-
-        def iloc(index, i):
-            if i < index.size:
-                return index[i]
-
-        while not df.empty and cutoff_time <= df.index[-1]:
-            if isinstance(self.window_size, int):
-                df_slice = df.iloc[:self.window_size]
-                window_end = iloc(df.index, self.window_size)
-
-            else:
-                window_end = cutoff_time + self.window_size
-                df_slice = df[:window_end]
-
-                # Pandas includes both endpoints when slicing by time.
-                # This results in the right endpoint overlapping in consecutive data slices.
-                # Resolved by making the right endpoint exclusive.
-                # https://pandas.pydata.org/pandas-docs/version/0.19/gotchas.html#endpoints-are-inclusive
-
-                if not df_slice.empty:
-                    overlap = df_slice.index == window_end
-                    if overlap.any():
-                        df_slice = df_slice[~overlap]
-
-            df_slice.context.window = (cutoff_time, window_end)
-
-            if isinstance(gap, int):
-                gap_end = iloc(df.index, gap)
-                df_slice.context.gap = (cutoff_time, gap_end)
-                df = df.iloc[gap:]
-
-                if not df.empty:
-                    cutoff_time = df.index[0]
-
-            else:
-                gap_end = cutoff_time + gap
-                df_slice.context.gap = (cutoff_time, gap_end)
-                cutoff_time += gap
-
-                if cutoff_time <= df.index[-1]:
-                    df = df[cutoff_time:]
-
-            if df_slice.empty and drop_empty:
-                continue
-
-            df.context.slice_number += 1
-
-            yield df_slice
-
     def slice(self, df, num_examples_per_instance, minimum_data=None, gap=None, drop_empty=True, verbose=False):
         """Generates data slices of target entity.
 
@@ -224,14 +91,20 @@ class LabelMaker:
             ds (generator): Returns a generator of data slices.
         """
         self._check_example_count(num_examples_per_instance, gap)
-        self.window_size = self.window_size or len(df)
-        gap = to_offset(gap or self.window_size)
-        groups = self.set_index(df).groupby(self.target_entity)
+        # self.window_size = self.window_size or len(df)
+        # gap = to_offset(gap or self.window_size)
+        df = self.set_index(df)
+        entity_groups = df.groupby(self.target_entity)
+        num_examples_per_instance = ExampleSearch._check_number(num_examples_per_instance)
+        
+        generator = DataSliceGenerator(
+            window_size=self.window_size or len(df),
+            min_data=minimum_data,
+            drop_empty=drop_empty,
+            gap=gap,
+        )
 
-        if num_examples_per_instance == -1:
-            num_examples_per_instance = float('inf')
-
-        for key, df in groups:
+        for key, df in entity_groups:
             slices = self._slice_by_time(df=df, gap=gap, min_data=minimum_data, drop_empty=drop_empty)
 
             for ds in slices:
@@ -359,9 +232,6 @@ class LabelMaker:
         """
         assert self.labeling_function, 'missing labeling function(s)'
         self._check_example_count(num_examples_per_instance, gap)
-        # self.window_size = self.window_size or len(df)
-        # gap = to_offset(gap or self.window_size)
-
         is_label_search = isinstance(num_examples_per_instance, dict)
         search = (LabelSearch if is_label_search else ExampleSearch)(num_examples_per_instance)
 
@@ -377,9 +247,6 @@ class LabelMaker:
             data_slice_generator=generator,
             search=search,
             verbose=verbose,
-            # gap=gap,
-            # min_data=minimum_data,
-            # drop_empty=drop_empty,
             *args,
             **kwargs,
         )
