01ed642f865f284876eb82e7f8c3112ad40fa577
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-07-06 16:43:05 -0500

    tests data slice offsets

--- composeml/tests/test_data_slice/test_offset.py
+++ composeml/tests/test_data_slice/test_offset.py
@@ -1,6 +1,19 @@
 from composeml.data_slice.offset import DataSliceOffset
+from pytest import raises
 
 
 def test_numeric_typecast():
     assert int(DataSliceOffset('1 nanosecond')) == 1
     assert float(DataSliceOffset('1970-01-01')) == 0.0
+
+
+def test_invalid_value():
+    with raises(AssertionError, match='invalid offset'):
+        DataSliceOffset(None)
+
+
+def test_alias_phrase():
+    phrase = 'until start of next month'
+    actual = DataSliceOffset(phrase).value
+    expected = DataSliceOffset('1MS').value
+    assert actual == expected
