9df9fd67c92750c5880d7f575c0b70a7603d4208
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-08-12 13:33:59 -0500

    rename packages

--- docs/source/examples/demo/next_purchase/__init__.py
+++ docs/source/examples/demo/next_purchase/__init__.py
@@ -2,11 +2,11 @@ import os
 import pandas as pd
 import requests
 import tarfile
-from demo import PWD, utils
+from demo import utils
 from tqdm import tqdm
 
 URL = r'https://s3.amazonaws.com/instacart-datasets/instacart_online_grocery_shopping_2017_05_01.tar.gz'
-PWD = os.path.join(PWD, 'next_purchase')
+PWD = os.path.dirname(__file__)
 
 
 def add_time(df, start='2015-01-01'):
