e877d8c073b896d3eecc2944c89c7e0a488e4630
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2021-06-07 15:27:33 -0500

    codecov ignore update checker

--- composeml/__init__.py
+++ composeml/__init__.py
@@ -1,15 +1,5 @@
 # flake8:noqa
-from pkg_resources import iter_entry_points
-
-from . import demos
+from . import demos, update_checker
 from .label_maker import LabelMaker
 from .label_times import LabelTimes, read_label_times
 from .version import __version__
-
-for entry_point in iter_entry_points('alteryx_open_src_initialize'):
-    try:
-        method = entry_point.load()
-        if callable(method):
-            method('composeml')
-    except Exception:
-        pass
