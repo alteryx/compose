3411ff671039a69479a0f2a02ac1f61f6afe479e
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2021-06-07 15:22:15 -0500

    iterate entry point

--- composeml/__init__.py
+++ composeml/__init__.py
@@ -1,5 +1,15 @@
 # flake8:noqa
+from pkg_resources import iter_entry_points
+
 from . import demos
 from .label_maker import LabelMaker
 from .label_times import LabelTimes, read_label_times
 from .version import __version__
+
+for entry_point in iter_entry_points('alteryx_open_src_initialize'):
+    try:
+        method = entry_point.load()
+        if callable(method):
+            method('composeml')
+    except Exception:
+        pass
