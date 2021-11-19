7ec02ec0dec570db4098451b002ab786263e07f1
Gaurav Sheni <gvsheni@gmail.com>
2021-11-19 12:01:38 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+from os import path
+
 from setuptools import find_packages, setup
+
+dirname = path.abspath(path.dirname(__file__))
+with open(path.join(dirname, 'README.md')) as f:
+    long_description = f.read()
+
 
 extras_require = {
     'update_checker': ['alteryx-open-src-update-checker >= 2.0.0'],
@@ -11,12 +18,26 @@ setup(
     author_email='support@featurelabs.com',
     license='BSD 3-clause',
     url='https://compose.alteryx.com',
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
+         'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     tests_require=open('test-requirements.txt').readlines(),
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    packages=find_packages(),
-    python_requires='>=3.7',
-    include_package_data=True,
+    python_requires='>=3.7, <4',
     extras_require=extras_require,
+    keywords='data science machine learning typing',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'woodwork = woodwork.__main__:cli'
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
