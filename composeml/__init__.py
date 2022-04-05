a61c897e41194d429fefa45a5f1b763c36015192
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-04-05 10:56:42 -0400

    fix version

--- composeml/__init__.py
+++ composeml/__init__.py
@@ -1,5 +1,5 @@
 # flake8:noqa
+from .version import __version__
 from . import demos, update_checker
 from .label_maker import LabelMaker
 from .label_times import LabelTimes, read_label_times
-from .version import __version__
